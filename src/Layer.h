ebec2cf15725a61c0532d9e99c8648fc553c5d52
Tim Kuipers <t.kuipers@ultimaker.com>
2015-10-12 13:48:19 +0200

    moved old src back to main src dir

--- src/Layer.h
+++ src/Layer.h
@@ -1,28 +1,83 @@
 #ifndef LAYER_H
 #define LAYER_H
 
-#include "opencv2/imgproc/imgproc.hpp"
-#include "Neuron.h"
+#include <cassert> // assert
+#include <vector>
+#include <cmath> // tanh
+#include <math.h>       /* exp */
 
-using namespace cv;
+#include "Mat3Df.h"
 
-class Layer 
+class Layer
 {
-public:
-    std::vector<Neuron> neurons;
     
+    std::vector<Mat3Df> weights;
+    std::vector<Mat3Df> weights_derivatives;
+    std::vector<float> biases;
+    std::vector<float> biases_derivatives;
     
-    Layer(int depth, int input_patch_size, int n_input_channels)
+    class State
     {
-        while (neurons.size() < depth) 
+//         Mat3Df input;
+        Mat3Df activation;
+        Mat3Df convoluted;
+        Mat3Df pooled;
+        
+        Mat3Df pooled_derivatives;
+        Mat3Df convoluted_derivatives;
+        
+        unsigned int pool_size = 2;
+        
+        
+        float soft_arg_max_abs(Mat3Df::Pos conv_pos, unsigned int w, unsigned int h, unsigned int z, float hardness)
         {
-            neurons.emplace_back(input_patch_size, n_input_channels);
+            double denom = 0;
+            double total = 0;
+            for (unsigned int x = 0; x < w; x++)
+            for (unsigned int y = 0; y < h; y++)
+            {
+                double in = convoluted.get(x, y, z);
+                double w = std::exp(hardness * std::abs(in));
+                total += w * in;
+                denom += w;
+            }
+            return total / denom;
         }
-    }
-    
-    
-
-
+        
+        void pool()
+        {
+            assert(pooled.d == convoluted.d 
+                && pooled.w == convoluted.w / pool_size
+                && pooled.h == convoluted.h / pool_size);
+            for (int x = 0; x < convoluted.x; x++)
+            for (int y = 0; y < convoluted.h; y++)
+            for (int z = 0; z < convoluted.d; z++)
+            {
+                pooled.set(x, y, z, soft_arg_max_abs(Mat3Df::Pos(x*pool_size, y*pool_size, z), pool_size, pool_size, 1));
+            }
+        }
+        
+        void signal(Mat3Df input)
+        {
+            auto signallingFunction = std::tanh; //[] (float in) { return 0; };
+            
+            for (int neuron = 0; neuron < weights.size(); neuron++)
+            {
+                input.convolute(weights[neuron], activation, neuron);
+                for (int x = 0; x < convoluted.x; x++)
+                for (int y = 0; y < convoluted.h; y++)
+                for (int z = 0; z < convoluted.d; z++)
+                {
+                    activation.add(x, y, z, biases[z]);
+                }
+                
+                activation.apply(signallingFunction, convoluted);
+                
+                pool();
+            }
+        }
+    };
 };
 
-#endif // LAYER_H
+
+#endif//LAYER_H
