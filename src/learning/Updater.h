21c70d95286a8a4b42449d02a4c7c8197462f4f5
Tim Kuipers <t.kuipers@ultimaker.com>
2015-11-02 12:15:19 +0100

    Updater

---
+++ src/learning/Updater.h
@@ -0,0 +1,53 @@
+#ifndef LEARNING_UPDATER_H
+#define LEARNING_UPDATER_H
+
+#include "../Network.h"
+#include "../layer/SubLayer.h"
+#include "../utils/Pos.h"
+
+/*!
+ * Class for updating the parameters of the network
+ */
+class Updater
+{
+    /*!
+     * Meta parameters such as the current velocity for momentum
+     */
+    virtual void initializeMetaParams(Network& network) = 0;
+    
+    virtual void update(Network& network) = 0;
+};
+
+class GradientDescentUpdater : public Updater
+{
+    double delta;
+    
+    GradientDescentUpdater(double delta)
+    : delta(delta)
+    {
+    }
+    
+    void initializeMetaParams(Network& network)
+    {
+        // gradient descent has no meta parameters
+    }
+    
+    void update(Network& network)
+    {
+        for (SubLayer* layer : network.layers)
+        {
+            for (LayerParams& params : layer->layer_params)
+            {
+                for (Pos4 p : params.dims)
+                {
+                    params.params.add(p, params.ders.get(p) * delta);
+                }
+            }
+        }
+    }
+    
+    
+    
+};
+
+#endif // LEARNING_UPDATER_H
