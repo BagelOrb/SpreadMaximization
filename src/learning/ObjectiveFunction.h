b753eae38bac3522c5df2bea9758b43b0b52cb54
Tim Kuipers <t.kuipers@ultimaker.com>
2015-11-02 12:15:06 +0100

    Objective function

---
+++ src/learning/ObjectiveFunction.h
@@ -0,0 +1,29 @@
+#ifndef LEARNING_OBJECTIVE_FUNCTION_H
+#define LEARNING_OBJECTIVE_FUNCTION_H
+
+#include "../Network.h"
+
+class ObjectiveFunction
+{
+protected:
+    virtual void setOutputDerivatives(Mat3Df& out, Mat3Df& out_ders) = 0;
+public:
+    void setOutputDerivatives(Network& network)
+    {
+        LayerState& layer_state = network.network_state.layer_states.back();
+        setOutputDerivatives(layer_state.output, layer_state.output_ders);
+    }
+
+};
+
+class BSobjectiveFunction : public ObjectiveFunction
+{
+protected:
+    void setOutputDerivatives(Mat3Df& out, Mat3Df& out_ders)
+    {
+        out_ders.clear();
+        out_ders.set(Pos3(0,0,0), 1.0);
+    }
+};
+
+#endif // LEARNING_OBJECTIVE_FUNCTION_H
