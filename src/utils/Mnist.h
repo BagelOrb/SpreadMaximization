9d56f6488557f56804c6a96f58b1334301e49392
Tim Kuipers <t.kuipers@ultimaker.com>
2015-11-16 15:07:46 +0100

    Mnist dataset reader

---
+++ src/utils/Mnist.h
@@ -0,0 +1,127 @@
+#ifndef UTILS_MNIST_H
+#define UTILS_MNIST_H
+
+#include <fstream>
+
+#include <iomanip> // std cerr precision TODO remove line
+
+#include "NoCopy.h"
+#include "Dims.h"
+#include "Mat3Df.h"
+#include "Mat4Df.h"
+
+class Mnist : NoCopy
+{
+    static constexpr int32_t magic_label = 2049;
+    static constexpr int32_t magic_data = 2051;
+
+    static Mnist* mnist;
+    
+public:
+    static Mnist& getMnist() 
+    { 
+        if (!mnist)
+        {
+            mnist = new Mnist();
+        }
+        return *mnist; 
+    }
+    
+private:
+    Mat4Df* data;
+    Mat4D<char>* labels;
+
+    static int32_t readInt32(std::fstream& fs)
+    {
+        int32_t num;
+        fs.read((char*)&num, sizeof(int32_t));
+        return __builtin_bswap32(num);
+    }
+    static unsigned char readInt8(std::fstream& fs)
+    {
+        unsigned char num;
+        fs.read((char*)&num, sizeof(char));
+        return num;
+    }
+    
+    static Dims4 readHeader(std::fstream& fs)
+    {
+        int w, h, d, m;
+        d = 1;
+        int32_t magic = readInt32(fs);
+        assert(magic == magic_data || magic == magic_label);
+        m = readInt32(fs);
+        if (magic == magic_data)
+        {
+            w = readInt32(fs);
+            h = readInt32(fs);
+        }
+        else 
+        {
+            w = h = 1;
+        }
+        return Dims4(w, h, d, m);
+    }
+
+    void readData()
+    {
+        std::fstream fs;
+        fs.open("data/MNIST/train-images.idx3-ubyte", std::fstream::in);
+
+        Dims4 dims = readHeader(fs);
+        
+        data = new Mat4Df(dims);
+        
+        for (unsigned int pixel_idx = 0 ; pixel_idx < data->size ; pixel_idx++)
+        {
+            data->data[pixel_idx] = readInt8(fs);
+        }
+        fs.close();
+        
+    }
+
+    void readLabels()
+    {
+        std::fstream fs;
+        fs.open("data/MNIST/train-labels.idx1-ubyte", std::fstream::in);
+
+        Dims4 dims = readHeader(fs);
+        
+        labels = new Mat4D<char>(dims);
+        
+        for (unsigned int pixel_idx = 0 ; pixel_idx < labels->size ; pixel_idx++)
+        {
+            labels->data[pixel_idx] = readInt8(fs);
+        }
+        fs.close();
+        
+    }
+    
+    Mnist()
+    : data(nullptr)
+    {
+        std::cerr << "reading MNIST data..." << std::endl;
+        readData();
+        std::cerr << "finished reading MNIST data.\n";
+        readLabels();
+        std::cerr << std::setprecision(0);
+        (*data)[0].debugOut("first image");
+        std::cerr << "label: " << int(labels->get(Pos4(0,0,0,0))) << std::endl;
+    }
+
+
+
+
+
+
+
+
+
+
+};
+
+
+Mnist* Mnist::mnist = nullptr;
+
+
+#endif // UTILS_MNIST_H
